1254b3a1e76d2a1ca3f40b7a1740fda7716c7fea
LukaJanjic <janjicluka@gmail.com>
2026-08-25 23:37:25 +0200

    add rounding tests for negative amounts and midpoint modes

--- tests/FxRateService.UnitTests/Money/RoundingTests.cs
+++ tests/FxRateService.UnitTests/Money/RoundingTests.cs
@@ -11,4 +11,27 @@ public class RoundingTests
 
         Assert.Equal(2.35m, result);
     }
+
+    [Fact]
+    public void ToScale_negativan_iznos_bezi_od_nule()
+    {
+        var result = Rounding.ToScale(-2.345m, 2, MidpointRounding.AwayFromZero);
+
+        Assert.Equal(-2.35m, result);
+    }
+
+    [Fact]
+    public void ToScale_bankarsko_zaokruzuje_na_paran()
+    {
+        var result = Rounding.ToScale(2.345m, 2, MidpointRounding.ToEven);
+
+        Assert.Equal(2.34m, result);
+    }
+
+    [Fact]
+    public void ToScale_negativna_skala_baca_izuzetak()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => Rounding.ToScale(1.23m, -1, MidpointRounding.AwayFromZero));
+    }
 }
